a2ac6a67c010223ff2a44195929966fb1e065886
rasmeister <rodstever@hotmail.com>
2015-05-27 08:49:38 -0400

    More clean up

--- prime/prime.js
+++ prime/prime.js
@@ -2,10 +2,19 @@
  * Created by RodS on 27/05/2015.
  */
 
-function generate_primes(form) {
-    var count = parseInt($("#prime_number_count").val());
-    var prime_numbers = [2, 3, 5, 7, 11, 13];
-    var result = prime_numbers.slice(0,count);
-    $("#results").html("<p>" + result.toString() + "</p>");
-    return false;   // prevent form submission
-}
+(function(prime, $) {
+    $(function() {
+        var generate_primes = function(form) {
+            var count = parseInt($("#prime_number_count").val());
+            $("#results").html("<p>" + getPrimeNumbers(count) + "</p>");
+            return false;   // prevent form submission
+        };
+
+        var getPrimeNumbers = function(count) {
+            var prime_numbers = [2, 3, 5, 7, 11, 13];
+            var result = prime_numbers.slice(0,count);
+            return result.toString();
+        };
+        prime.generate_primes = generate_primes;
+    });
+})(window.prime = window.prime || {}, jQuery);
