0aeaf41b9dccf48bf9d1e58b72a96e90be3d2454
rasmeister <rodstever@hotmail.com>
2015-05-27 07:58:30 -0400

    Add gitignore and clean up basic layout

--- prime/prime.js
+++ prime/prime.js
@@ -2,7 +2,10 @@
  * Created by RodS on 27/05/2015.
  */
 
-function generate_primes() {
-    $("<p>2, 3, 5, 7, 11, 13</p>").appendTo("body");
+function generate_primes(form) {
+    var count = parseInt($("#prime_number_count").val());
+    var prime_numbers = [2, 3, 5, 7, 11, 13];
+    var result = prime_numbers.slice(0,count);
+    $("#results").html("<p>" + result.toString() + "</p>");
     return false;   // prevent form submission
 }
