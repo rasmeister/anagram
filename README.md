c908e9f308bd308118df5c31c7e67a10f9780920
rasmeister <rodstever@hotmail.com>
2015-05-27 07:36:39 -0400

    Initial html to support prime number generator

---
+++ README.md
@@ -0,0 +1,3 @@
+##Running the Prime Number Generator
+
+Open the testPrime.html by dragging the file from explorer to a browser window.
---
+++ prime/prime.js
@@ -0,0 +1,8 @@
+/**
+ * Created by RodS on 27/05/2015.
+ */
+
+function generate_primes() {
+    $("<p>2, 3, 5, 7, 11, 13</p>").appendTo("body");
+    return false;   // prevent form submission
+}
